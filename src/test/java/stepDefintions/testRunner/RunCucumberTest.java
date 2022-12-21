5d3c9be099af641e5ff7d158195eeef123302d5b
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-21 01:11:57 +0100

    runner add

---
+++ src/test/java/stepDefintions/testRunner/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package stepDefintions.testRunner;
+
+
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasspathResource("features")
+public class RunCucumberTest {
+}
